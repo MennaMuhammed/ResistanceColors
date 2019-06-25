43cb69dfb6fe09144bdd668a3955db984bf06a13
gassersamy <gassersamy7@gmail.com>
2019-06-25 14:14:57 +0200

    timer code added

--- Assets/gamecontrol.cs
+++ Assets/gamecontrol.cs
@@ -9,25 +9,32 @@ public class gamecontrol : MonoBehaviour
     public TextMeshProUGUI value;
     public TextMeshProUGUI goal;
     public TextMeshProUGUI score;
+    public TextMeshProUGUI timer;
     public Resistance resistance;
 
     private float randGoal;
     private int score_num=0;
-    float[] factors = new float[] {10f, 100f, 1000f, 10000f, 100000f, 1000000f,10000000f,100000000f, 1f, 0.1f, 0.001f};
+    float[] factors = new float[] {10f, 100f, 1000f, 10000f, 100000f, 1000000f,10000000f,100000000f, 1f, 0.1f, 0.01f};
+
+    //timer
+    private float timer_val = 120; //2 minutes
+    private bool canCount=true;
+    private bool doOnce=false;
 
     // Start is called before the first frame update
     void Start()
     {
         updategoal();
         //InvokeRepeating("updategoal",15,15);
-        InvokeRepeating("checkScore",1f,1f);
+        InvokeRepeating("checkScore",1f,0.5f);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        value.text= resistance.value.ToString();
+        value.text= resistance.value.ToString("N");
+        countDown();
         //checkScore(); 
     }
 
@@ -36,9 +43,11 @@ public class gamecontrol : MonoBehaviour
 
         int r = UnityEngine.Random.Range(0, factors.Length);
         randGoal = UnityEngine.Random.Range(1,20) * factors[r];
-        goal.text= randGoal.ToString();
+        goal.text= randGoal.ToString("N");
+        reset_timer();
         
     }
+    
 
     void checkScore()
     {
@@ -51,6 +60,37 @@ public class gamecontrol : MonoBehaviour
             value.text="0";
             
         }
+    }
+
+
+    void countDown()
+    {
+        if(timer_val >= 0.0f && canCount)
+        {
+            timer_val -= Time.deltaTime;
+            int seconds = (int) (timer_val % 60);
+            int minutes = (int) (timer_val / 60 ) % 60;
+
+            string timeformat = string.Format("{0:00}:{1:00}", minutes,seconds);
+            timer.text=timeformat;
+        }
+
+        else if(timer_val<= 0.0f && ! doOnce)
+        {
+            canCount=false;
+            doOnce=true;
+            timer.text="00:00";
+            timer_val=0;
+            Debug.Log("Game Over");
+        }
+    }
+
+
+    void reset_timer()
+    {
+        timer_val=120;
+        canCount=true;
+        doOnce=false;
     }
 
 }
